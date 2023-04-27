209ea24af2269285ef178ed67aa80207bb0fbdc0
sapomh <61649616+sapomh@users.noreply.github.com>
2023-04-27 21:14:13 +0300

    adding cors

--- fyp/fyp/settings.py
+++ fyp/fyp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'fyp.urls'
@@ -109,7 +110,10 @@ REST_FRAMEWORK = {
     ]
 }
 
-
+CORS_ALLOWED_ORIGINS = [
+    'http://192.168.1.4:3000'
+]
+CORS_ALLOW_ALL_ORIGINS = True
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
